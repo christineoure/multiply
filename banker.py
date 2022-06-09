31b9de8abd755d55ca3f3463a311befb7b706c31
christineoure <ourechristine@gmail.com>
2022-06-09 22:22:08 +0300

    commit changes

--- banker.py
+++ banker.py
@@ -3,17 +3,23 @@ class Bank:
         self.account_number = account_number
         self.account_name = account_name
         self.balance=0
+        self.deposits=[]
+        self.withdrawals=[]
+
         
 
     def deposit (self, amount):
         self.balance+= amount
         return f"you have deposited this amount{amount} and this is your balance {self.balance}"
 
+
     def deposit (self, amount):
         if amount <=0:
-            return f"Deposit akmount should be more than zero"
-            
+            return f"Deposit amount should be more than zero"
+
         self.balance += amount
+        self.deposits.append(amount) 
+        print(self.deposits)
         return f"You have deposited {amount}. Your new balance is {self.balance}"
 
     def withdraw (self, amount):
@@ -23,7 +29,14 @@ class Bank:
             return f"Amount must be greater than zero"
         else:
             self.balance-=amount
-            return f"You have withdrawn {amount}. Your balance is {self.balance}"
+            self.withdrawals.append(amount)
+            return f"You have withdrawn {amount}. Your balance is {self.balance}", self.withdrawals
+
+    def deposits_statements():
+        print(*self.deposits,sep="/n")
+
+    def withdrawals_statements():
+        print(*self.withdrawals,sep="/n")
         
         
         
